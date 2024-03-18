3be6478e779a6c0dfb0d84a2b5e1c622aaeae099
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-03-18 20:45:22 +0100

    fix jwt-strategy & add controllers guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserController } from './user/user.controller';
 
 @Module({
   imports: [
@@ -12,5 +13,6 @@ import { PrismaModule } from './prisma/prisma.module';
     AuthModule,
     PrismaModule,
   ],
+  controllers: [UserController],
 })
 export class AppModule {}
