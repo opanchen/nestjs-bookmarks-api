bab91da510041b778b472204fb9c58b4ff32b18d
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-03-18 22:36:42 +0100

    add auth e2e-tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,9 +4,15 @@ import {
   INestApplication,
   ValidationPipe,
 } from '@nestjs/common';
+import * as pactum from 'pactum';
+// import { PrismaService } from '@/prisma/prisma.service';
+import { PrismaService } from '../src/prisma/prisma.service';
+
+import { AuthDto } from 'src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeAll(async () => {
     const moduleRef =
@@ -23,6 +29,15 @@ describe('App e2e', () => {
     );
 
     await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+
+    await prisma.cleanDb();
+
+    pactum.request.setBaseUrl(
+      'http://localhost:3333',
+    );
   });
 
   afterAll(() => {
@@ -30,4 +45,99 @@ describe('App e2e', () => {
   });
 
   it.todo('should pass');
+
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'oleh@gmail.com',
+      password: '123',
+    };
+
+    describe('Signup', () => {
+      it('should throw an exception if email is empty', async () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: '123',
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw an exception if password is empty', async () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: 'oleh@gmail.com',
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw an exception if no body provided', async () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .expectStatus(400);
+      });
+
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Signin', () => {
+      it('should throw an exception if email is empty', async () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: '123',
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw an exception if password is empty', async () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: 'oleh@gmail.com',
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw an exception if no body provided', async () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .expectStatus(400);
+      });
+
+      it('should signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200);
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {});
+
+    describe('Edit user', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+    describe('Get bookmarks', () => {});
+    describe('Get bookmark by id', () => {});
+    describe('Edit bookmark', () => {});
+    describe('Delete bookmark', () => {});
+  });
 });
